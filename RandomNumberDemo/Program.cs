696fe83b0949b20094430943de186d6245b9a6ec
Tony Watkins <tony@llifion.net>
2020-09-03 14:17:03 +0100

    Add project files.

---
+++ RandomNumberDemo/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace RandomNumberDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            Console.ReadLine();
+        }
+    }
+}
