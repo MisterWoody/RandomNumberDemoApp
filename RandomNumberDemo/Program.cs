8266f301496d7d1756723bb9f36b78446bd9fa8b
Tony Watkins <tony@llifion.net>
2020-09-03 14:35:05 +0100

    Generate numbers from 0 to just less than 10,

--- RandomNumberDemo/Program.cs
+++ RandomNumberDemo/Program.cs
@@ -6,14 +6,21 @@ namespace RandomNumberDemo
     {
         static void Main(string[] args)
         {
-            Random random = new Random(1);
+            Random random = new Random();
 
             for (int i = 0; i < 10; i++)
             {
-                Console.WriteLine(random.Next(5,11));
+                //Console.WriteLine(random.Next(5,11));
+                Console.WriteLine(random.NextDouble() * 10);
+                //SimpleMethod(random); // Best practice - do this, don't create a new object each time
             }
 
             Console.ReadLine();
+        }
+
+        private static void SimpleMethod(Random random)
+        {
+            Console.WriteLine(random.Next()); // Beware - do NOT create a new random object here with a seed as that leads to potential security holes,
         }
     }
 }
